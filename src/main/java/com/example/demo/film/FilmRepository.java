54931f07c6fd199f0d87f380e6bf52b571a4f957
Sean Carr <35836323+LiquidAkiba@users.noreply.github.com>
2022-08-25 22:34:02 +0100

    code smell in FilmRepo done

--- src/main/java/com/example/demo/film/FilmRepository.java
+++ src/main/java/com/example/demo/film/FilmRepository.java
@@ -2,7 +2,6 @@ package com.example.demo.film;
 
 import org.springframework.data.repository.CrudRepository;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 public interface FilmRepository extends CrudRepository<Film,Integer> {
@@ -11,5 +10,5 @@ public interface FilmRepository extends CrudRepository<Film,Integer> {
 
     List<Film> findByFilmsActorsActorId(int id);
 
-    //List<Film> getRentalRate (BigDecimal rental_rate);
+
 }
