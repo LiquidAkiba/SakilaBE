f7e2336eee62f370f50a6dd3118444162ae6c1e3
Sean Carr <35836323+LiquidAkiba@users.noreply.github.com>
2022-08-25 20:58:47 +0100

    code smell clean up on main file.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,20 +1,12 @@
 package com.example.demo;
 
-import com.example.demo.actor.Actor;
 import com.example.demo.actor.ActorRepository;
-import com.example.demo.category.Category;
 import com.example.demo.category.CategoryRepository;
-import com.example.demo.film.Film;
 import com.example.demo.film.FilmRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
-
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
 
 @CrossOrigin(origins="*")
 @SpringBootApplication
