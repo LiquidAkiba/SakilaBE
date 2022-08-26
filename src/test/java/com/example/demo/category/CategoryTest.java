e50bc38a01f86a53431590b050be3ccfb8a5a456
Sean Carr <35836323+LiquidAkiba@users.noreply.github.com>
2022-08-26 14:49:39 +0100

    sonarcloud fix

--- src/test/java/com/example/demo/category/CategoryTest.java
+++ src/test/java/com/example/demo/category/CategoryTest.java
@@ -1,5 +1,6 @@
 package com.example.demo.category;
 
+import com.example.demo.actor.Actor;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,10 +9,17 @@ class CategoryTest {
 
     @Test
     void getCategoryId() {
+
+        Category testCategory = new Category();
+        testCategory.setCategoryId(23);
+        assertEquals(23, testCategory.getCategoryId());
     }
 
     @Test
     void setCategoryId() {
+        Category testCategory = new Category();
+        testCategory.setCategoryId(23);
+        assertEquals(23, testCategory.getCategoryId());
     }
 
     @Test
@@ -23,5 +31,8 @@ class CategoryTest {
 
     @Test
     void setName() {
+        Category testCategory = new Category();
+        testCategory.setName("RomCom");
+        assertEquals("RomCom", testCategory.getName(), "The category did not match");
     }
 }
